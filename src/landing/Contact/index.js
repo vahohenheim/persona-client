618bc9620ef2bdf11c23e7c143b2d1e42773dd47
Valentin Bourreau <valentin.bourreau@gmail.com>
2019-11-29 11:45:10 +0100

    add responsive design

--- src/landing/Contact/index.js
+++ src/landing/Contact/index.js
@@ -9,11 +9,11 @@ const Contact = () => {
     return (
         <Container>
             <Row name="contact" type="flex" justify="center" style={{ textAlign: 'center', padding: '80px 0' }}>
-                <Col span={14}>
+                <Col span={20} sm={14}>
                     <Heading tag="h2">Contact me</Heading>
                     <p>If you have a project, a opportunity to me or just talk about somthing, please feel free to contact me.</p>
                 </Col>
-                <Col span={22} lg={16}>
+                <Col span={24} lg={16}>
                     <FormContact />
                 </Col>
             </Row>
